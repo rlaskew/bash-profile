4bbd893faf5acadc32c16acd8d055d574de75a1a
Richard Askew <richardaskew@yahoo.com>
2022-02-11 20:44:24 +0000

    creating env step 1

--- aws-cloudshell.sh
+++ aws-cloudshell.sh
@@ -1,7 +1,50 @@
-#!/bin/bash
+#!/bin/bash +x
 
+## repo name
+REPO_NAME=bash-profile
 ## preferred editor mode
 set -o vi
+
+## git
+git config --global user.email "richardaskew@yahoo.com" && git config --global user.name "Richard Askew"
+
+## add path
+export PATH=${PATH}:~/${REPO_NAME}:${REPO_NAME}/bin
+
+## add some aliases
+# some more ls aliases
+alias ll='ls -alh'
+alias la='ls -A'
+alias l='ls -CFlh'
+alias woo='fortune'
+alias lsd="ls -alF | grep /$"
+
+# This is GOLD for finding out what is taking so much space on your drives!
+alias diskspace="du -S | sort -n -r |more"
+
+# Command line mplayer movie watching for the win.
+alias mp="mplayer -fs"
+
+# Show me the size (sorted) of only the folders in this directory
+alias folders="find . -maxdepth 1 -type d -print | xargs du -sk | sort -rn"
+
+# This will keep you sane when you're about to smash the keyboard again.
+alias frak="fortune"
+
+# aws aliases
+alias acf='aws cloudformation'
+
+## man pages
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;31m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;32m'
+
+## also applies to most
+# export PAGER="/usr/bin/most -s"
 
 ## Say Hello 
 echo "Hello"

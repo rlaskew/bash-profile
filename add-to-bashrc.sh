e70b4b3ca367573f546240b48288300992eb6024
Richard Askew <richardaskew@yahoo.com>
2022-03-15 16:41:08 +0000

    Checkpoint

---
+++ add-to-bashrc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "set -o vi" >> ~/.bashrc
+. ~/.bashrc
---
+++ helpful-commands.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git config --global user.email "richardaskew@yahoo.com" && git config --global user.name "Richard Askew"

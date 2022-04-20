c272935f59dd3c9fea78662dadcd9ca2d0068c42
rlaskew <richardaskew@yahoo.com>
2022-04-20 11:40:55 -0500

    Update add-to-bashrc.sh

--- add-to-bashrc.sh
+++ add-to-bashrc.sh
@@ -6,3 +6,4 @@ echo "set -o vi" >> ~/.bashrc
 set -o vi
 ## from https://git-scm.com/docs/git-credential-cache
 git config --global credential.helper 'cache --timeout 3600'
+git config --global user.email "richardaskew@yahoo.com" && git config --global user.name "Richard Askew"

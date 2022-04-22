ad9280336a10be8275ebf1930db006aa2da11313
rlaskew <richardaskew@yahoo.com>
2022-04-22 07:29:45 -0500

    Update gcp-sandbox-setup.sh

--- gcp-sandbox-setup.sh
+++ gcp-sandbox-setup.sh
@@ -9,7 +9,7 @@ git config --global credential.helper 'cache --timeout 3600'
 git config --global user.email "richardaskew@yahoo.com" && git config --global user.name "Richard Askew"
 
 # clone common repos
-git clone https://github.com/rlaskew/bash-profile
+# git clone https://github.com/rlaskew/bash-profile
 git clone https://github.com/rlaskew/gcp-workspace
 # git clone https://github.com/rlaskew/inferno-workspace
 # git clone https://github.com/rlaskew/edge-workspace
